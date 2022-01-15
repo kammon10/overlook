0c11bdd377d481ecd30403c0b54ca078ede2147a
Katie Ammon <katie.ammon10@gmail.com>
2022-01-15 07:30:00 -0700

    Add customer test and class

--- src/scripts.js
+++ src/scripts.js
@@ -4,9 +4,7 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
-import {
-  showCalander,
-} from './domUpdates';
+import './domUpdates';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
